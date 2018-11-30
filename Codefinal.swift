1eac0c7319de8de396c36028bc395f30324d0e4d
jessangel79 <babin.angelique@gmail.com>
2018-11-30 15:38:00 +0100

    Création d'un programme en swift

---
+++ Codefinal.swift
@@ -0,0 +1,11 @@
+// Age of the people
+
+let age: Int = 18
+
+if age >= 21 {
+    print("Vous êtes majeur, et même aux Etat-Unis !")
+} else if age >= 18 {
+    print("Vous êtes majeur, mais pas aux Etat-Unis. :(")
+} else {
+    print("Vous êtes mineur.")
+}
